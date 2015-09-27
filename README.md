3bbd58f25d75a1c0d6d638b1d90b1e9978bfa46f
Eric Freeman <efreeman@1800contacts.com>
2015-09-27 13:57:36 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# git-files
+Find all files of type between two commits.  Used at work to find updated sql files since previous release so we can replicate changes to production easier.
---
+++ src/git-files/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LibGit2Sharp;
+
+namespace git_fukd
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var changes = new Dictionary<string, int>();
+            var files = new List<string>();
+            var spinner = new ConsoleSpiner();
+            Console.Write("Working....");
+
+            var directory = Directory.GetCurrentDirectory();
+            directory = "C:\\home\\projects\\git-fukd";
+            using (var repo = new Repository(directory))
+            {
+                var commits = repo.Commits;
+                Commit lastCommit = null;
+
+                var start = repo.Tags.First().Target.Sha;
+                var end = repo.Tags.Last().Target.Sha;
+
+                var isLooking = false;
+                var isLast = false;
+
+                foreach (var commit in commits)
+                {
+                    spinner.Turn();
+
+                    var tree = commit.Tree;
+
+                    if (lastCommit == null)
+                    {
+                        lastCommit = commit;
+                        continue;
+                    }
+
+                    var parentCommitTree = lastCommit.Tree;
+                    lastCommit = commit;
+
+                    if (commit.Sha == start || commit.Sha == end)
+                    {
+                        isLooking = !isLooking;
+                        isLast = !isLooking;
+                    }
+
+                    if (isLooking)
+                    {
+                        var c = repo.Diff
+                            .Compare<TreeChanges>(parentCommitTree, tree)
+                            .ToList();
+
+                        c.ForEach(commitChanges =>
+                        {
+                            var file = commitChanges.Path;
+                            if (file.EndsWith(".sql") && !files.Contains(file))
+                            {
+                                files.Add(file);
+                            }
+                        });
+                    }
+
+                    if (isLast)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine();
+
+                foreach (var change in files)
+                {
+                    Console.WriteLine(change);
+                }
+            }
+        }
+    }
+}
